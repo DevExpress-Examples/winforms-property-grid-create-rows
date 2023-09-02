1e4f4a2bdd48aae9aebbe5e89c20b42124a462b0
DevExpressExampleBot <fakeEmail>
2023-09-02 17:48:37 +0400

    Source auto update [skip ci]

--- VB/AddRowsRuntime/Form1.Designer.vb
+++ VB/AddRowsRuntime/Form1.Designer.vb
@@ -1,58 +1,55 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace AddRowsRuntime
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.propertyGridControl1 = New DevExpress.XtraVerticalGrid.PropertyGridControl()
-			CType(Me.propertyGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' propertyGridControl1
-			' 
-			Me.propertyGridControl1.Location = New System.Drawing.Point(13, 13)
-			Me.propertyGridControl1.Name = "propertyGridControl1"
-			Me.propertyGridControl1.Size = New System.Drawing.Size(319, 200)
-			Me.propertyGridControl1.TabIndex = 0
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(523, 226)
-			Me.Controls.Add(Me.propertyGridControl1)
-			Me.Name = "Form1"
-			Me.Text = "How to create PropertyGridControl rows at runtime"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.propertyGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.propertyGridControl1 = New DevExpress.XtraVerticalGrid.PropertyGridControl()
+            CType((Me.propertyGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' propertyGridControl1
+            ' 
+            Me.propertyGridControl1.Location = New System.Drawing.Point(13, 13)
+            Me.propertyGridControl1.Name = "propertyGridControl1"
+            Me.propertyGridControl1.Size = New System.Drawing.Size(319, 200)
+            Me.propertyGridControl1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(8F, 16F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(523, 226)
+            Me.Controls.Add(Me.propertyGridControl1)
+            Me.Name = "Form1"
+            Me.Text = "How to create PropertyGridControl rows at runtime"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.propertyGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private propertyGridControl1 As DevExpress.XtraVerticalGrid.PropertyGridControl
-	End Class
+#End Region
+        Private propertyGridControl1 As DevExpress.XtraVerticalGrid.PropertyGridControl
+    End Class
 End Namespace
-
--- VB/AddRowsRuntime/Form1.vb
+++ VB/AddRowsRuntime/Form1.vb
@@ -1,83 +1,89 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraVerticalGrid.Rows
 
 Namespace AddRowsRuntime
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			CreateRows()
+    Public Partial Class Form1
+        Inherits Form
 
-			Dim person As New Person()
-			person.LastName = "Doe"
-			person.FirstName = "John"
-			propertyGridControl1.SelectedObject = person
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub CreateRows()
-			Dim rowLastName As New EditorRow("LastName")
-			rowLastName.Properties.Caption = "Last name"
-			propertyGridControl1.Rows.Add(rowLastName)
-			Dim rowCategory As New CategoryRow("Address")
-			propertyGridControl1.Rows.Add(rowCategory)
-			Dim rowAddressLine1 As New EditorRow("AddressLine1")
-			rowAddressLine1.Properties.Caption = "Address Line 1"
-			rowCategory.ChildRows.Add(rowAddressLine1)
-			Dim rowZip As New EditorRow("Zip")
-			rowZip.Properties.Caption = "Zip Code"
-			rowCategory.ChildRows.Add(rowZip)
-		End Sub
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            CreateRows()
+            Dim person As Person = New Person()
+            person.LastName = "Doe"
+            person.FirstName = "John"
+            propertyGridControl1.SelectedObject = person
+        End Sub
 
-	Public Class Person
-		Private _FirstName As String
-		Public Property FirstName() As String
-			Get
-				Return _FirstName
-			End Get
-			Set(ByVal value As String)
-				_FirstName = value
-			End Set
-		End Property
+        Private Sub CreateRows()
+            Dim rowLastName As EditorRow = New EditorRow("LastName")
+            rowLastName.Properties.Caption = "Last name"
+            propertyGridControl1.Rows.Add(rowLastName)
+            Dim rowCategory As CategoryRow = New CategoryRow("Address")
+            propertyGridControl1.Rows.Add(rowCategory)
+            Dim rowAddressLine1 As EditorRow = New EditorRow("AddressLine1")
+            rowAddressLine1.Properties.Caption = "Address Line 1"
+            rowCategory.ChildRows.Add(rowAddressLine1)
+            Dim rowZip As EditorRow = New EditorRow("Zip")
+            rowZip.Properties.Caption = "Zip Code"
+            rowCategory.ChildRows.Add(rowZip)
+        End Sub
+    End Class
 
-		Private _LastName As String
-		Public Property LastName() As String
-			Get
-				Return _LastName
-			End Get
-			Set(ByVal value As String)
-				_LastName = value
-			End Set
-		End Property
+    Public Class Person
 
-		Private _AddressLine1 As String
-		Public Property AddressLine1() As String
-			Get
-				Return _AddressLine1
-			End Get
-			Set(ByVal value As String)
-				_AddressLine1 = value
-			End Set
-		End Property
+        Private _FirstName As String
 
-		Private _Zip As String
-		Public Property Zip() As String
-			Get
-				Return _Zip
-			End Get
-			Set(ByVal value As String)
-				_Zip = value
-			End Set
-		End Property
-	End Class
+        Public Property FirstName As String
+            Get
+                Return _FirstName
+            End Get
+
+            Set(ByVal value As String)
+                _FirstName = value
+            End Set
+        End Property
+
+        Private _LastName As String
+
+        Public Property LastName As String
+            Get
+                Return _LastName
+            End Get
+
+            Set(ByVal value As String)
+                _LastName = value
+            End Set
+        End Property
+
+        Private _AddressLine1 As String
+
+        Public Property AddressLine1 As String
+            Get
+                Return _AddressLine1
+            End Get
+
+            Set(ByVal value As String)
+                _AddressLine1 = value
+            End Set
+        End Property
+
+        Private _Zip As String
+
+        Public Property Zip As String
+            Get
+                Return _Zip
+            End Get
+
+            Set(ByVal value As String)
+                _Zip = value
+            End Set
+        End Property
+    End Class
 End Namespace
